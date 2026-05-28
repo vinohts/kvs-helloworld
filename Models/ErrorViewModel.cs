2543567eba7047de8ac4375c7d87d696713c90c5
vinohts <vino.hts@gmail.com>
2026-05-28 13:31:30 +0530

    Initial Hello World ASP.NET Core MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace KvsHelloWorld.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
